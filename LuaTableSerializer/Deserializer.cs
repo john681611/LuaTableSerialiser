adecc81124886a711adbc01cc07f95b654ca6f9b
john681611 <john681611@hotmail.com>
2025-06-24 13:11:48 +0100

    Update Deserializer to handle special characters in strings and keys

--- LuaTableSerializer/Deserializer.cs
+++ LuaTableSerializer/Deserializer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json;
@@ -9,11 +10,26 @@ namespace LuaTableSerializer
     {
         internal static Dictionary<object, object> ToDict(string data)
         {
+            // TODO FIND AND REPLACE ALL STRINGS WITH GUID REFS
+            var replaceTable = new Dictionary<string, string>();
+            var strings = Regex.Matches(data, @"""(.*?(?<!\\))""");
+            foreach (Match match in strings)
+            {
+                var guid = System.Guid.NewGuid().ToString();
+                data = data.Replace(match.Value, $"\"{guid}\"");
+                replaceTable[guid] = match.Value.Trim('"');
+            }
+
             var keyReplace = Regex.Replace(data, "\\[(.+?)\\] = ", "$1:");
             var digitFix = Regex.Replace(keyReplace, "(\\d+):", "\"~$1\":");
             var removeSpace = Regex.Replace(digitFix, ",(\\s*})", "$1");
+            foreach (var kv in replaceTable)
+            {
+                removeSpace = removeSpace.Replace($"\"{kv.Key}\"", $"\"{kv.Value}\"");
+            }
             var deserializedData = JsonConvert.DeserializeObject<Dictionary<string, object>>(removeSpace);
             return ConvertTypes(deserializedData);
+
         }
 
         private static Dictionary<object, object> ConvertTypes(Dictionary<string, object> data)
--- tests/DeserializerTests.cs
+++ tests/DeserializerTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using FluentAssertions;
 using Newtonsoft.Json;
@@ -33,11 +34,67 @@ namespace LuaTableSerializer.Tests
         }
 
         [Fact]
+        public void Deserializer_Dict_EscapedQuotation_ReturnString()
+        {
+            var result = LuaSerializer.Deserialize(@"{[""Bob""] = ""Item1"",[""Jeff""] = ""Item2 (\""Jezza])"",}");
+            var jsonResult = JsonConvert.SerializeObject(result);
+            jsonResult.Should().Be(@"{""Bob"":""Item1"",""Jeff"":""Item2 (\""Jezza])""}");
+        }
+
+        [Fact]
         public void Deserializer_NestedDict_ReturnString()
         {
             var result = LuaSerializer.Deserialize(@"{[""Bob""] = {[""Bob""] = ""Item1"",[""Jeff""] = ""Item2"",},[""Jeff""] = {[""Bob""] = ""Item1"",[""Jeff""] = ""Item2"",},}");
             var jsonResult = JsonConvert.SerializeObject(result);
             jsonResult.Should().Be(@"{""Bob"":{""Bob"":""Item1"",""Jeff"":""Item2""},""Jeff"":{""Bob"":""Item1"",""Jeff"":""Item2""}}");
         }
+
+        [Fact]
+        public void Deserializer_RealWorldExample_ReturnString()
+        {
+            var result = LuaSerializer.Deserialize(@"{
+        [""name""] = ""AH-64D_BLK_II"",
+        [""payloads""] = {
+                [1] = {
+                        [""displayName""] = ""2 * M261: A/B - M151 (6PD), E - M257 (6IL), 2 * Hellfire station: 4*AGM-114K"",
+                        [""name""] = ""2 * M261: A/B - M151 (6PD), E - M257 (6IL), 2 * Hellfire station: 4*AGM-114K"",
+                        [""pylons""] = {
+                                [1] = {
+                                        [""CLSID""] = ""{M261_OUTBOARD_AB_M151_E_M257}"",
+                                        [""num""] = 4,
+                                },
+                                [2] = {
+                                        [""CLSID""] = ""{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}"",
+                                        [""num""] = 3,
+                                },
+                                [3] = {
+                                        [""CLSID""] = ""{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}"",
+                                        [""num""] = 2,
+                                },
+                                [4] = {
+                                        [""CLSID""] = ""{M261_OUTBOARD_AB_M151_E_M257}"",
+                                        [""num""] = 1,
+                                },
+                                [5] = {
+                                        [""CLSID""] = ""{AN_APG_78}"",
+                                        [""num""] = 6,
+                                },
+                        },
+                        [""tasks""] = {
+                                [1] = 16,
+                                [2] = 30,
+                                [3] = 31,
+                                [4] = 18,
+                                [5] = 32,
+                        },
+                },
+        },
+        [""unitType""] = ""AH-64D_BLK_II"",
+
+}");
+            var jsonResult = JsonConvert.SerializeObject(result);
+            jsonResult.Should().Be(@"{""name"":""AH-64D_BLK_II"",""payloads"":{""1"":{""displayName"":""2 * M261: A/B - M151 (6PD), E - M257 (6IL), 2 * Hellfire station: 4*AGM-114K"",""name"":""2 * M261: A/B - M151 (6PD), E - M257 (6IL), 2 * Hellfire station: 4*AGM-114K"",""pylons"":{""1"":{""CLSID"":""{M261_OUTBOARD_AB_M151_E_M257}"",""num"":4},""2"":{""CLSID"":""{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}"",""num"":3},""3"":{""CLSID"":""{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}"",""num"":2},""4"":{""CLSID"":""{M261_OUTBOARD_AB_M151_E_M257}"",""num"":1},""5"":{""CLSID"":""{AN_APG_78}"",""num"":6}},""tasks"":{""1"":16,""2"":30,""3"":31,""4"":18,""5"":32}}},""unitType"":""AH-64D_BLK_II""}");
+        }
+
     }
 }
